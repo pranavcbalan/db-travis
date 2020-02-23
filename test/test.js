6d9290c82c63ec7b00d5104db89db70ce55c9dd7
pranavc <pranavcbalan@gmail.com>
2020-02-23 20:40:54 +0530

    first commit

---
+++ test/test.js
@@ -0,0 +1,52 @@
+const {expect} = require('chai');
+
+//desribe group of tests done
+describe('Describe Test Scenarios', function () {
+
+  let knexMysql;
+  let knexPg;
+
+  before(function (done) {
+    //start appln
+    knexMysql = require('knex')({
+      client: 'mysql',
+      connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'chinook'
+      }
+    });
+    //start appln
+    knexPg = require('knex')({
+      client: 'pg',
+      connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '',
+        database: 'chinook'
+      }
+    });
+
+    done();
+  });
+
+  it('Describe test - mysql', async function (done) {
+    //testcase
+
+    let res = await knexMysql('Album').select('*');
+
+    expect(res.length).to.not.equal(0)
+    done();
+  });
+
+  it('Describe test - pg', async function (done) {
+    //testcase
+
+    let res = await knexPg('Album').select('*');
+
+    expect(res.length).to.not.equal(0)
+    done();
+  });
+
+});
